88f108eb7407fcf51dd79caaf707a4649fb181ed
Dispin123 <Dispin123@gmail.com>
2026-05-19 10:14:52 -0400

    Update LeadForm.tsxfix: remove stray h from onSuccess TS type - fixes TypeScript compile error

--- letendretech/components/LeadForm.tsx
+++ letendretech/components/LeadForm.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 interface LeadFormProps {
   compact?: boolean;
   defaultService?: string;
-  onSuccess?: () => void;h
+  onSuccess?: () => void;
 }
 
 const SERVICE_OPTIONS = [
